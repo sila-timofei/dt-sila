a2e3902cee117cf5e8aa5de106aa826c436feb26
Timofei <sila.timofey@gmail.com>
2021-11-16 19:42:35 +0300

    Add contact

--- js/main.js
+++ js/main.js
@@ -1,41 +1,9 @@
-const containerQuestions = document.querySelector('.questions');
+window.addEventListener('DOMContentLoaded', () => {
+    const offLink = document.querySelectorAll('.off-link');
 
-function closeAllQuestions() {
-    containerQuestions
-        .querySelectorAll('.questions__body')
-        .forEach(qBody => {
-            qBody.style.cssText = ``;
-            qBody
-                .querySelector('.questions__btn')
-                .classList
-                .remove('questions__btn_close');
+    offLink.forEach(i => {
+        i.addEventListener('click', e => {
+            e.preventDefault();
         });
-}
-
-containerQuestions
-    .querySelectorAll('.questions__body')
-    .forEach(qBody => {
-        qBody
-            .querySelector('.questions__title')
-            .addEventListener('click', event => {
-                let isOnlyClose = event
-                                    .target
-                                    .classList
-                                    .contains('questions__btn_close');
-
-                closeAllQuestions();
-
-                if (isOnlyClose) return;
-
-                const questionsBtn = qBody.querySelector('.questions__btn');
-                const h = qBody
-                            .querySelector('.questions__text')
-                            .clientHeight + 90;
-
-                qBody.style.cssText = `height: ${h}px;`;
-            
-                questionsBtn
-                    .classList
-                    .add('questions__btn_close');
-             });
     });
+});
